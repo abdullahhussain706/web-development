8e544c26a4ac12f664939811c25c39d6342d2abc
abdullahhussain706 <abdullah706706@gmail.com>
2025-01-15 18:43:54 +0500

    script in js file

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,2 +1,14 @@
-let currDisplay='12345'
-document.querySelector('#display').innerHTML=currDisplay;
+let currDisplay=''
+function valueAdd(value){
+    currDisplay+=value;
+    document.querySelector('#display').value=currDisplay;
+
+}
+function calculate(){
+    currDisplay=eval(currDisplay);
+    document.querySelector('#display').value=currDisplay;
+}
+function clearDisplay() {
+    currDisplay = '';
+    document.querySelector('#display').value = currDisplay;
+}
